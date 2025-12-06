4552cce69ce3000b8c993bde4f43cc32e6875cca
siushin <siushin@163.com>
2025-12-07 00:50:48 +0800

    安装 vue-router、pinia，移除多余代码

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import Antd from 'ant-design-vue'
 import App from './App.vue'
 import 'ant-design-vue/dist/reset.css'
 
+const pinia = createPinia()
 const app = createApp(App)
 
-app.use(Antd).mount('#app')
+app.use(Antd)
+    .use(pinia)
+    .mount('#app')
